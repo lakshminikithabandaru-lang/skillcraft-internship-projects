82b725af7c0370e5a8f448391e952bd60acf39f0
lakshminikithabandaru-lang <lakshminikithabandaru@gmail.com>
2026-06-05 18:41:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,72 @@
-function addTask() {
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
 
-    let taskInput = document.getElementById("taskInput");
-    let taskDate = document.getElementById("taskDate");
-    let taskList = document.getElementById("taskList");
+let timer = null;
 
-    if (taskInput.value === "") {
-        alert("Please enter a task");
+function updateDisplay() {
+
+    let h = String(hours).padStart(2, '0');
+    let m = String(minutes).padStart(2, '0');
+    let s = String(seconds).padStart(2, '0');
+
+    document.getElementById("display").innerText =
+        h + ":" + m + ":" + s;
+}
+
+function stopwatch() {
+
+    seconds++;
+
+    if (seconds == 60) {
+        seconds = 0;
+        minutes++;
+    }
+
+    if (minutes == 60) {
+        minutes = 0;
+        hours++;
+    }
+
+    updateDisplay();
+}
+
+function startTimer() {
+
+    if (timer !== null) {
         return;
     }
 
-    let li = document.createElement("li");
-
-    li.innerHTML = `
-        ${taskInput.value} - ${taskDate.value}
-        <button onclick="completeTask(this)">Complete</button>
-        <button onclick="editTask(this)">Edit</button>
-        <button onclick="deleteTask(this)">Delete</button>
-    `;
-
-    taskList.appendChild(li);
-
-    taskInput.value = "";
-    taskDate.value = "";
+    timer = setInterval(stopwatch, 1000);
 }
 
-function completeTask(button) {
-    button.parentElement.style.textDecoration = "line-through";
+function pauseTimer() {
+
+    clearInterval(timer);
+    timer = null;
 }
 
-function editTask(button) {
-    let li = button.parentElement;
-    let newTask = prompt("Edit Task", li.firstChild.textContent);
+function resetTimer() {
 
-    if (newTask) {
-        li.firstChild.textContent = newTask;
-    }
+    clearInterval(timer);
+
+    timer = null;
+
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+
+    updateDisplay();
+
+    document.getElementById("laps").innerHTML = "";
 }
 
-function deleteTask(button) {
-    button.parentElement.remove();
+function lapTimer() {
+
+    let lap = document.createElement("li");
+
+    lap.innerText =
+        document.getElementById("display").innerText;
+
+    document.getElementById("laps").appendChild(lap);
 }
